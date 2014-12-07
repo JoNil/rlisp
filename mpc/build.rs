916114f5f8108ee029e1e898e909f10ad9d6f5d2
Jonathan Nilsson <jonathan@voysys.se>
2014-12-07 18:43:45 +0100

    Changed to build mpc with gcc-rs

--- mpc/build.rs
+++ mpc/build.rs
@@ -1,23 +1,9 @@
-use std::io::Command;
-use std::io::process::ProcessExit;
+extern crate gcc;
+
+use std::default::Default;
 use std::os;
 
 fn main() {
-    let out_dir = os::getenv("OUT_DIR").unwrap();
-
-    let output = match Command::new("make")
-            .arg("all")
-            .cwd(&Path::new("src/"))
-            .output() {
-        Ok(output) => output,
-        Err(e) => panic!("Failed to run make: {}", e),
-    };
-
-    if ProcessExit::ExitStatus(0) != output.status {
-        println!("stdout: {}", String::from_utf8_lossy(output.output.as_slice()));
-        println!("stderr: {}", String::from_utf8_lossy(output.error.as_slice()));
-        panic!("Failed to compile mpc");
-    }
-
-    println!("cargo:rustc-flags=-L {} -l mpc:static", out_dir);
+    os::setenv("CFLAGS", "-ansi -std=c99 -pedantic -pedantic-errors -Wall -Werror");
+    gcc::compile_library("libmpc.a", &Default::default(), &["src/mpc.c"]);
 }
--- rlisp/src/lib.rs
+++ rlisp/src/lib.rs
@@ -1,5 +1,4 @@
 #![feature(globs)]
-#![feature(if_let)]
 #![feature(phase)]
 #![feature(slicing_syntax)]
 
