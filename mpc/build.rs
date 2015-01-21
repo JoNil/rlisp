426183f4905ffbface6bb91e95e6085daa31da57
Jonathan Nilsson <lord.jonil@gmail.com>
2015-01-21 06:33:59 +0100

    Added optimizations for mpc

--- mpc/build.rs
+++ mpc/build.rs
@@ -6,6 +6,6 @@ use std::default::Default;
 use std::os;
 
 fn main() {
-    os::setenv("CFLAGS", "-ansi -std=c99 -pedantic -pedantic-errors -Wall -Werror");
+    os::setenv("CFLAGS", "-ansi -std=c99 -O3 -pedantic -pedantic-errors -Wall -Werror");
     gcc::compile_library("libmpc.a", &Default::default(), &["src/mpc.c"]);
 }
